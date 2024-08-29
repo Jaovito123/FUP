afd0f460b146d279d94c6fb46ebccc6f4398b79e
Jaovito123 <163334671+Jaovito123@users.noreply.github.com>
2024-08-28 22:02:17 -0300

    Delete Estoque directory

--- Estoque/main.py
+++
@@ -1,7 +0,0 @@
-import funcoes
-
-funcoes.adicionar("Laticinios", "queijo", 4)
-funcoes.verificar_itens()
-funcoes.adicionar("Graos", "Arroz", 2)
-funcoes.remover("Laticinios", "queijo", 4)
-funcoes.verificar_itens()
